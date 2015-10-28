1c043432b4da70c578edf0b021d69e56e6d385e1
zaifoski <sofiachimirri@gmailcom>
2015-10-28 17:24:16 +0100

    implement the game

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # trentose-memory
 A simulation exam for the students of Software Engineering II - UNITN
 
-*Matricola* : Your student ID
+*Matricola* : 168358
 
-*Nome*      : Your name surname
+*Nome*      : Sofia Chimirri
--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /* your code should go here */
-
+var tmpl="<li id=\"N\" class=\"done\">"+
+         "<h3>NUM</h3> "   +
+         "</li>";
 
 // Three main classes of cards:
 // done: when the user has discovered the pair of cards
@@ -7,6 +9,33 @@
 
 $(document).ready(function(){
 
+    for(var i=0; i< data.length; i++){
+      var item = tmpl.replace("N", data[i]-1);
+      item = item.replace("NUM", data[i]);
+      $(".cards").append(item);
+    }
+    $(".opt-start").click(function(){
+        for(var i=0; i< data.length; i++){
+          $("#"+i).attr('class',"");
+          $("#"+i).attr('class',"hidden");
+        }
+    });
+    var counter = 0;
+    // clicked function
+    $(".cards").on("click", "li", function(){
+        var clicked = $(this).attr("id");
+        if(clicked==counter){          
+            $(this).attr('class',"");
+            $(this).attr('class',"done");
+            if(counter==9){
+                alert("You won!");
+            }
+        }
+        else{
+            alert("You lost!");
+        }
+        counter +=1;
+    });
   
 });
 
